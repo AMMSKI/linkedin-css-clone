8784bb5af03351569e2e5a473e849e5b70cd95dc
Alan Mamulski <alanmamulski@gmail.com>
2021-12-09 21:49:51 -0700

    nav setup

---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { GrLinkedin } from 'react-icons/gr'
+
+const Navbar = () => {
+  return (
+  <div className='nav_container'>
+    <div>
+      <GrLinkedin size='35px' color='#0B65C2'/>
+      <p>search</p>
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+  </div>
+  )
+}
+
+export default Navbar
